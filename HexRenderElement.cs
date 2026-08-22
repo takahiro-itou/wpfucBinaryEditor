f777a6adba18cf52bfcafed2d64c3df7490d7289
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-22 11:32:38 +0900

    空のクラスを新規作成

---
+++ HexRenderElement.cs
@@ -0,0 +1,31 @@
+
+//  -*-  coding: utf-8-with-signature  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  WPF UserControl Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+using System;
+
+
+namespace  WpfControl.Editor  {
+
+//========================================================================
+//
+//    HexRenderElement  class
+//
+
+public  class  HexRenderElement
+{
+
+}   //  End class  HexRenderElement
+
+}   //  End of namespace  WpfControl.Editor
