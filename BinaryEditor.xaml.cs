a18efc522d2872a71a7d138505ab670c6e67f724
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-22 13:26:19 +0900

    コンパイルエラーを修正

--- BinaryEditor.xaml.cs
+++ BinaryEditor.xaml.cs
@@ -12,7 +12,7 @@
 **                                                                      **
 *************************************************************************/
 
-using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 
 
